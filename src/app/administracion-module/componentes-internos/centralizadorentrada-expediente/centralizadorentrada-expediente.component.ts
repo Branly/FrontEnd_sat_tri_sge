f8927d70fae763068e0a80c7c8e6956d8663abdf
Alex Estrada <aestrada@diamante.com.gt>
2022-07-07 08:30:30 -0600

    creación componente centralizador

--- src/app/administracion-module/componentes-internos/centralizadorentrada-expediente/centralizadorentrada-expediente.component.ts
+++ src/app/administracion-module/componentes-internos/centralizadorentrada-expediente/centralizadorentrada-expediente.component.ts
@@ -1,17 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-centralizadorentrada-expediente',
   templateUrl: './centralizadorentrada-expediente.component.html',
   styleUrls: ['./centralizadorentrada-expediente.component.css']
 })
+
 export class CentralizadorentradaExpedienteComponent implements OnInit {
 
   displayedColumns: string[] = [
-    'noexpendiente', 'descripcion', 'nombrecontribuyente', 'nit', 'fechatributa', 'gerenciaorigen', 'folios', 'observaciones', 
+    'noexpediente', 'tiporecurso', 'nombrecontribuyente', 'nit', 'fechatributa', 'gerenciaorigen', 'folios', 'observaciones', 
     'noexpedientetributa', 'resolucion', 'direccionfiscal', 'cantidadajustes'
   ];
+  dataSource = new MatTableDataSource();
   mostrarTablaPadre = true;
+
+  @ViewChild('MatPaginator1') set matPaginator(mp1: MatPaginator) {
+    this.dataSource.paginator = mp1;
+  }  
 
   constructor() { }
 
